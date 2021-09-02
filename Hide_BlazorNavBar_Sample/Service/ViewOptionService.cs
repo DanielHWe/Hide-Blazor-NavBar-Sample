49a765785dbf6cfe00b1a7d8e483759229195d50
Wehrle, Daniel <WehrleD@haufe.com>
2021-09-02 09:12:29 +0200

    Add project files.

---
+++ Hide_BlazorNavBar_Sample/Service/ViewOptionService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hide_BlazorNavBar_Sample.Service
+{
+    public class ViewOptionService
+    {
+        private bool _navBarVisible = true;
+
+        public Action OnChanged { get; set; }
+
+        public bool IsNavigationVisible { get { return _navBarVisible; } }
+
+        //Change state by click on the button
+        public void Toggle()
+        {
+            _navBarVisible = !_navBarVisible;//Change
+            if (OnChanged != null) OnChanged();//Callback for reload
+        }
+
+        //get additional css class for nav bar div
+        public string NavBarClass
+        {
+            get
+            {
+                if (_navBarVisible) return String.Empty;//No additional css class for show nav bar
+                return "d-none";//d-none class will hide the div
+            }
+        }
+    }
+}
